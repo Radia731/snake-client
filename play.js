fda3584124c4d0e59c09734681b64552545176d3
Radia Ahmed <radiajjr17@gmail.com>
2023-04-23 21:18:33 -0400

    connecting with the server

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: "50541", // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {  // this line listens for the "data" event on the connectionz
+    console.log("Server says: ", data);
+  });
+
+  conn.on("idle", () => {
+
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
