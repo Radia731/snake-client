1ddac82485cb9d36872ef31bed3edb1b23a5edac
Radia Ahmed <radiajjr17@gmail.com>
2023-04-25 05:30:57 -0400

    update

--- client.js
+++ client.js
@@ -1,22 +1,26 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: "50541", // PORT number here,
+    host: IP , // IP address here,
+    port: PORT, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {  // this line listens for the "data" event on the connectionz
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("name: RAD");
+  });
+
+  conn.on("data", (data) => {
+    // this line listens for the "data" event on the connectionz
     console.log("Server says: ", data);
   });
 
-  conn.on("idle", () => {
-
-  });
+  conn.on("idle", () => {});
 
   return conn;
 };
--- play.js
+++ play.js
@@ -1,5 +1,7 @@
-const { host, port } = require(".play");
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-const conn = connect();
+connect();
+
+setupInput();
